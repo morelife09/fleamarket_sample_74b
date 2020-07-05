dd6075b86957bfc02d21e228cbd9655f61266f3f
morelife09 <junichi1995930@gmail.com>
2020-07-05 16:41:18 +0900

    commit3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,13 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
+
+  def production?
+    Rails.env.production?
+  end
+
 end
