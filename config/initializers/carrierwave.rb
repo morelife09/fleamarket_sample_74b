d6aa795b82a5d7052e00dbd4ea9c0d586f5665a9
morelife09 <junichi1995930@gmail.com>
2020-07-12 20:59:32 +0900

    commit3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id], 123
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key], 345
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'fleamarketsample74b'
