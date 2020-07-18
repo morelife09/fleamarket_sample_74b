f814c5b66278474834fa24d25a7c3413f041b661
morelife09 <junichi1995930@gmail.com>
2020-07-18 11:43:03 +0900

    commit2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
+  get 'users#show'
+  resources :users, only: [:show]
   devise_for :users
   root 'items#index'
   get 'items', to: 'items#index'
+
 end
