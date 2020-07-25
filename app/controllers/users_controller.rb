e591e9c64d2d8073f1446ba4b31597a5267c6799
morelife09 <junichi1995930@gmail.com>
2020-07-25 17:00:23 +0900

    commit4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :mypage]
 
   def show
+  end
+
+  def mypage
   end
 
   private
 
   def set_user
     @user = User.find([:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'users#show'
+  root 'users#show'
   resources :users, only: [:show]
   devise_for :users
+  root 'users#mypage'
+  get 'mypage', to: 'users#mypage'
   root 'items#index'
   get 'items', to: 'items#index'
 
