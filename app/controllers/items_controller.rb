b19053820d6340b9363e67dbf2af1e21b2d8247b
toi1989 <migiwanagisa@gmail.com>
2020-08-02 17:55:21 +0900

    クリエイト部分修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    # if @item.save
   #     redirect_to root_path
   #  　else
   #     redirect_to new_items_path
