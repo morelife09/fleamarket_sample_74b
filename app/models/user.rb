b8cce1335956fa70895e75af016130336800349e
toi1989 <migiwanagisa@gmail.com>
2020-08-12 18:32:57 +0900

    brand_table,user_table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :first_name, :family_name, :first_name_furigana, :family_name_furigana, :birth_date ,presence: true
-  has_many :items, dependent: :destroy
   has_many :credit_cards, dependent: :destroy
   has_one :delivery_information
   has_many :item_purchases, dependent: :destroy
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :selling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item", dependent: :destroy
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
 end
