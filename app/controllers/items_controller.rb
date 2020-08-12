c7ed0343fd04df8783e8b760db4847de7ea6b8a4
toi1989 <migiwanagisa@gmail.com>
2020-08-12 18:31:26 +0900

    item_table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,26 @@
 class ItemsController < ApplicationController
+  def index
+  end
 
   def new
     @item = Item.new
+    @item.images.build
   end
 
   def create
     @item = Item.new(item_params)
-    # if @item.save
-  #     redirect_to root_path
-  #  　else
-  #     redirect_to new_items_path
-  #  　end
-
+    if  @item.save
+       redirect_to items_path
+    else
+       render :new
+    end
   end
-  # params.require(:store).permit(:name, :address, :image, :image_cache)
  
   private
   def item_params
-    params.require(:item).permit(:name, :price, :description, :prefecture_id, :condition_id, :user_id, :category_id, :size_id, :shipping_id, :delivery_days_id, :price, :brand_id,)
+    params.require(:item).permit(:name, :price, :description, :prefecture_id, :seller_id, 
+    :buyer_id, :condition_id, :category_id, :size_id, :shipping_fee_id,
+     :delivery_days_id, :brand_id,  
+     images_attributes: [:src, :_destroy, :id]).merge(seller_id: current_user.id)
   end
-
 end
--- db/migrate/20200711032144_create_items.rb
+++ db/migrate/20200711032144_create_items.rb
@@ -1,17 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.references :user, foreignkey: true
+      t.integer :buyer_id, foreign_key: true
+      t.integer :seller_id , foreignkey: true
       t.references :category, foreignkey: true
-      t.string :name, null:false
-      t.string :price, null:false
+      t.string :name, null: false
+      t.string :price, null: false
       t.text :description, null: false
       t.integer :size_id
       t.integer :condition_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :shipping_fee_id, null: false
       t.integer :delivery_days_id, null: false 
-      t.references :brand
+      t.integer :brand_id
       t.timestamps
     end
   end
