8d5edf3a0d36ef7e6ba2795c9f9c701ba325f5e8
toi1989 <migiwanagisa@gmail.com>
2020-08-12 18:33:11 +0900

    image_table

--- db/migrate/20200801024715_create_images.rb
+++ db/migrate/20200801024715_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.references :item, foreign_key: true
-      t.string :image, null: false
+      t.string :src, null: false
       t.timestamps
     end
   end
