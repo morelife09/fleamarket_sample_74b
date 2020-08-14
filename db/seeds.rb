92bbedc54f3688349183e179f36be4cd7a582f09
case0101 <ksksakamoto01@gmail.com>
2020-08-14 16:21:43 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+parents = ["レディース","メンズ","ベビー・キッズ","インテリア・住まい・小物","本・音楽・ゲーム","おもちゃ・ホビー・グッズ","コスメ・香水・美容","家電・スマホ・カメラ","スポーツ・レジャー","ハンドメイド","チケット","自動車・オートバイ","その他"]
+
+parents.each do |parent|
+  Category.find_or_create_by(name: parent)
+end
