4eaece459d2907789966488884dfd0c5b3b44220
case0101 <ksksakamoto01@gmail.com>
2020-08-19 17:40:24 +0900

    Create credit_cards.js

---
+++ app/assets/javascripts/credit_cards.js
@@ -0,0 +1,32 @@
+$(document).on("turbolinks:load", function () {
+  if (!$("#regist_card")[0]) return false;
+
+  Payjp.setPublickey("pk_test_7359c79f1304d35944f10fbb");
+
+  $("#regist_card").on("click", function (e) {
+    e.preventDefault();
+    var card = {
+      number: $("#number").val(),
+      exp_month: $("#exp_month").val(),
+      exp_year: $("#exp_year").val(),
+      cvc: $("#cvc").val(),
+    };
+
+    Payjp.createToken(card, function (status, response) {
+      if (status === 200) {
+        $("#number").removeAttr("name");
+        $("#exp_month").removeAttr("name");
+        $("#exp_year").removeAttr("name");
+        $("#cvc").removeAttr("name");
+        var token = response.id;
+        $("#card_form").append(
+          `<input type="hidden" name="card_token" value=${token}>`
+        );
+        $("#card_form").get(0).submit();
+      } else {
+        alert("カード情報が正しくありません。");
+        $("#regist_card").prop("disabled", false);
+      }
+    });
+  });
+});
