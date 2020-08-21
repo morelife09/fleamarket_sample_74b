f40787238ebfad8dacd1636475bf52ac27cb4a79
case0101 <ksksakamoto01@gmail.com>
2020-08-22 06:54:30 +0900

    index credit card

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,7 +1,10 @@
 class CreditCardsController < ApplicationController
   require "payjp"
   def new
-
+    @card = CreditCard.where(user_id: current_user.id).first
+      if @card.present?
+        redirect_to action:"index"
+      end
   end
 
   def create
@@ -10,6 +13,7 @@ class CreditCardsController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
+        email: current_user.email,
         card: params[:card_token],
         metadata: {user_id: current_user.id}
       )
@@ -18,7 +22,38 @@ class CreditCardsController < ApplicationController
         customer_id: customer.id,
         user_id: current_user.id
       )
-      @card.save
+      if @card.save
+        redirect_to action: "index"
+      else
+        render "new"
+      end
+    end
+  end
+
+  def index
+    @card = CreditCard.where(user_id: current_user.id).first
+    if @card.present?
+      Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_info = customer.cards.retrieve(@card.card_id)
+      @brand = @default_card_info.brand
+      @exp_month = @default_card_info.exp_month.to_s
+      @exp_year = @default_card_info.exp_year.to_s.slice(2,3)
+
+      case @brand
+      when "Visa"
+        @card_image = "visa.svg"
+      when "JCB"
+        @card_image = "jcb.svg"
+      when "MasterCard"
+        @card_image = "mastercard.svg"
+      when "American Express"
+        @card_image = "american_express.svg"
+      when "Diners Club"
+        @card_image = "dinersclub.svg"
+      when "Discover"
+        @card_image = "discover.svg"
+      end
     end
   end
 end
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,4 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
+  validates :user_id, :customer_id, :card_id, presence: true
 end
