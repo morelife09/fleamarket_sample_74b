6a0c4f14e5ed63ee5679bf5741738c5118891ec3
case0101 <ksksakamoto01@gmail.com>
2020-08-28 06:28:57 +0900

    modify item purchase and credit card

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_items, only: [:show]
+  before_action :set_items, only: [:show, :purchase]
   before_action :set_categories, only: [:index, :new, :create, :show]
-  
+
   def index
   end
 
@@ -24,6 +24,29 @@ class ItemsController < ApplicationController
 
   def purchase
     @d_info = DeliveryInformation.find(current_user.id)
+    @card = CreditCard.find_by(user_id: current_user.id)
+    if @card.present?
+      Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_info = customer.cards.retrieve(@card.card_id)
+      @brand = @default_card_info.brand
+      @exp_month = @default_card_info.exp_month.to_s
+      @exp_year = @default_card_info.exp_year.to_s.slice(2,3)
+      case @brand
+      when "Visa"
+        @card_image = "visa.svg"
+      when "JCB"
+        @card_image = "jcb.svg"
+      when "MasterCard"
+        @card_image = "mastercard.svg"
+      when "American Express"
+        @card_image = "american_express.svg"
+      when "Diners Club"
+        @card_image = "dinersclub.svg"
+      when "Discover"
+        @card_image = "discover.svg"
+      end
+    end
   end
 
   def get_category
@@ -45,7 +68,7 @@ class ItemsController < ApplicationController
      :delivery_days_id, :brand_id,
      images_attributes: [:src, :_destroy, :id]).merge(seller_id: current_user.id)
   end
-  
+
   def set_items
     @item = Item.includes(:seller,:category).find(params[:id])
   end
