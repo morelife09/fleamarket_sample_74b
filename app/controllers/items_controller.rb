021985917ba68bbd9e22411b189c91fb3c3a4767
YoichiroKimoto <yokimot@gmail.com>
2020-08-29 14:42:42 +0900

    [WIP]Edit-items
    #What
    コードの見直しを実施
    
    #Why
    指摘箇所修正のため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,22 +29,10 @@ class ItemsController < ApplicationController
     grandchild_category = @item.category
     child_category = grandchild_category.parent
 
+    @category_parent_array = Category.where(ancestry: nil)
+    @category_children_array = Category.where(ancestry: child_category.ancestry)
+    @category_grandchildren_array = Category.where(ancestry: grandchild_category.ancestry)
 
-    @category_parent_array = []
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent
-    end
-    
-
-    @category_children_array = []
-    Category.where(ancestry: child_category.ancestry).each do |children|
-      @category_children_array << children
-    end
-
-    @category_grandchildren_array = []
-    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
-      @category_grandchildren_array << grandchildren
-    end
   end
 
   def update
