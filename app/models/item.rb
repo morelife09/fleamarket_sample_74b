9724ec9c2e18e34aa03c13a60a1ec7ad142eb72e
toi1989 <migiwanagisa@gmail.com>
2020-08-29 18:10:15 +0900

    js_rev

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,5 +14,5 @@ class Item < ApplicationRecord
   validates_associated :images
   validates :images, :name, :description, :price, :seller_id, :category_id, :condition_id, :prefecture_id,
   :shipping_fee_id, :delivery_days_id, :brand_id, presence: true
-  validates :price,numericality: { only_integer: true,greater_than: 300, less_than: 999999}
+  validates :price,numericality: { only_integer: true,greater_than: 299, less_than: 1000000}
 end
