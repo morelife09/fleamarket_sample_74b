c6e1f4712a6abf9bf184ca2ca7d5bed4ed9a5c3a
YoichiroKimoto <yokimot@gmail.com>
2020-09-05 20:23:07 +0900

    modify

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,33 @@
 class CommentsController < ApplicationController
+  before_action :set_item, only: [:create, :destroy]
   before_action :set_comment, only: [:destroy]
 
   def create
-    @item = Item.find(params[:item_id])
     @comment = Comment.new(comment_params)
-    comment = Comment.create(comment_params)
-    redirect_to @item
+    if @comment.save
+      redirect_to @item
+    else
+      flash[:alert] = "コメント出来ませんでした"
+      redirect_to root_path
+    end
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
-    @comment.destroy
-    redirect_to @item
+    if @comment.destroy
+      redirect_to @item
+    else
+      flash[:alert] = "削除出来ませんでした"
+      redirect_to root_path
+    end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text, :item_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def set_comment
